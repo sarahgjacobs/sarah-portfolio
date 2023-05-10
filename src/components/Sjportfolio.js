689bc60d2a3e9ab11c49eb368eb1a9d12cf0f0b1
Sarah Jacobs <sgjacobs14@gmail.com>
2023-05-10 11:16:57 -0400

    set up front page

--- src/components/Sjportfolio.js
+++ src/components/Sjportfolio.js
@@ -6,12 +6,26 @@ import { Link } from 'react-router-dom';
 
 
 function About() {
+  const showSecondSection = () => {
+    const secondSection = document.getElementById('welcome-page');
+    secondSection.style.display = 'block';
+  }
+
   return (
     <section>
-      <h1 id="falling-welcome">Welcome to my portfolio!</h1>
-      <button id="falling-button" class="button">
-        <Link to="/">Home</Link> | <Link to="/projects">Projects</Link> | <Link to="/contact">Contact</Link>
-      </button>
+      <h1 id="falling-welcome" onAnimationEnd={showSecondSection}></h1>
+
+      <section id="welcome-page">
+        <h1>Welcome to My Portfolio</h1>
+        <p>I'm a web developer based in [Location]. I love creating beautiful and functional websites that make a difference.</p>
+        <div>
+          <h2>Education</h2>
+          <h3>Georgia Tech Bootcamp</h3>
+          <h6>This is where I'll put the info about GT</h6>
+          <h3>Georgia College & State University</h3>
+          <h6>BA in Communication with a concentration in Digital Media</h6>
+        </div>
+      </section>
 
     </section>
   );
