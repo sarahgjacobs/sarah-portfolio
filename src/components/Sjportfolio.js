c69176874141f9ee8f69e396555bcecd400cd5f9
Sarah Jacobs <sgjacobs14@gmail.com>
2023-05-12 12:47:51 -0400

    testing photo

--- src/components/Sjportfolio.js
+++ src/components/Sjportfolio.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-
-
 
 
 function About() {
@@ -19,7 +15,7 @@ function About() {
         <h1>Welcome to My Portfolio!</h1>
         <h2>I'm Sarah Jacobs, a <i>Software Engineer</i> based in Atlanta, GA. I have a strong background in communication, digital media, and leadership which I've combined with a Coding certification from Georgia Tech Bootcamps. Now I am currently seeking roles in the tech industry ranging from Development to Databases. Click <a href="/about">here</a> to see more of my experience and languages, then head over to the <a href="/projects">projects</a> tab to see it all in action.</h2>
         <div id="picture-container">
-          <img src="/path/to/picture.jpg" alt="Profile picture" />
+          <img src="../img/profilepic.jpg" alt="Profile picture" />
         </div>
       </section>
 
