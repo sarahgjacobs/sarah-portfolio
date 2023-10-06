8952ff472bf007d84db46127cbdaac72debc028c
Sarah Jacobs <sgjacobs14@gmail.com>
2023-10-06 19:28:10 -0400

    fixed redirect

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,33 +3,33 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="navbar">
-      <h1 className="navbar__title">Sarah Jacobs</h1>
-      <ul className="navbar__links">
-        <li className="navbar__item">
-          <Link to="/" className="navbar__link">
-            Home
-          </Link>
-        </li>
-        <li className="navbar__item">
-          <Link to="/about" className="navbar__link">
-            About
-          </Link>
-        </li>
-        <li className="navbar__item">
-          <Link to="/projects" className="navbar__link">
-            Projects
-          </Link>
-        </li>
-        <li className="navbar__item">
-          <Link to="/contact" className="navbar__link">
-            Contact
-          </Link>
-        </li>
-      </ul>
-      <div className="navbar__bar"></div>
-    </nav>
-  );
+    // <nav className="navbar">
+    //   <h1 className="navbar__title">Sarah Jacobs</h1>
+    //   <ul className="navbar__links">
+    //     <li className="navbar__item">
+    //       <Link to="/" className="navbar__link">
+    //         Home
+    //       </Link>
+    //     </li>
+    //     <li className="navbar__item">
+    //       <Link to="/about" className="navbar__link">
+    //         About
+    //       </Link>
+    //     </li>
+    //     <li className="navbar__item">
+    //       <Link to="/projects" className="navbar__link">
+    //         Projects
+    //       </Link>
+    //     </li>
+    //     <li className="navbar__item">
+    //       <Link to="/contact" className="navbar__link">
+    //         Contact
+    //       </Link>
+    //     </li>
+    //   </ul>
+    //   <div className="navbar__bar"></div>
+    // </nav>
+  <div></div>);
 }
 
 export default Navbar;
--- src/components/Sjportfolio.js
+++ src/components/Sjportfolio.js
@@ -7,10 +7,10 @@ import { useEffect } from 'react';
 
 function About() {
   //falling animation for page loading
-  const showSecondSection = () => {
-    const secondSection = document.getElementById('welcome-page');
-    secondSection.style.display = 'block';
-  }
+  // const showSecondSection = () => {
+  //   const secondSection = document.getElementById('welcome-page');
+  //   secondSection.style.display = 'block';
+  // }
 
   function RedirectExample() {
     useEffect(() => {
@@ -26,45 +26,45 @@ function About() {
   RedirectExample()
 
   return (
-    <section>
-      <h1 id="falling-welcome" onAnimationEnd={showSecondSection}></h1>
+    // <section>
+    //   <h1 id="falling-welcome" onAnimationEnd={showSecondSection}></h1>
 
-      <section id="welcome-page">
-      <a target='_blank'
-            rel='noopener noreferrer' href="https://sarahgjacobs.github.io/sarahjacobsdev/">Portfolio</a>
+    //   <section id="welcome-page">
+    //   <a target='_blank'
+    //         rel='noopener noreferrer' href="https://sarahgjacobs.github.io/sarahjacobsdev/">Portfolio</a>
    
-        <h1 id="port"> <button id='port-button'><a href="https://sarahgjacobs.github.io/sarahjacobsdev/" className='new-port' id='port-button'>Click here for my newest portfolio</a></button></h1>
-        <h2>Hello, I'm Sarah Jacobs, a Software Developer based in Atlanta, GA, 
-          on a mission to blend the best of tech and creativity. With a knack 
-          for communication, a love for digital media, and a dash of teaching experience, 
-          I've infused my skills with a Coding certification from Georgia Tech 
-          Bootcamps. Now, I'm on the hunt for exciting opportunities in the tech 
-          world. Curious to know more? Swing by the 'About' page to discover my 
-          newest websites!</h2>
-        <br></br>
-        <a href={pdf} download>
-          <button className="download-button">Resume PDF</button>
-        </a>
-        <a href={resume} download>
-          <button className="download-button">Resume Docx</button>
-        </a>
+    //     <h1 id="port"> <button id='port-button'><a href="https://sarahgjacobs.github.io/sarahjacobsdev/" className='new-port' id='port-button'>Click here for my newest portfolio</a></button></h1>
+    //     <h2>Hello, I'm Sarah Jacobs, a Software Developer based in Atlanta, GA, 
+    //       on a mission to blend the best of tech and creativity. With a knack 
+    //       for communication, a love for digital media, and a dash of teaching experience, 
+    //       I've infused my skills with a Coding certification from Georgia Tech 
+    //       Bootcamps. Now, I'm on the hunt for exciting opportunities in the tech 
+    //       world. Curious to know more? Swing by the 'About' page to discover my 
+    //       newest websites!</h2>
+    //     <br></br>
+    //     <a href={pdf} download>
+    //       <button className="download-button">Resume PDF</button>
+    //     </a>
+    //     <a href={resume} download>
+    //       <button className="download-button">Resume Docx</button>
+    //     </a>
 
 
-        <br></br>
+    //     <br></br>
 
-        <ul id="hobbies">
-          <h3>In my free time I enjoy:</h3>
-          <li>Learning Korean</li>
-          <li>Writing Stories (<a href="https://sarahgjacobs.github.io/sarahjacobswrites/#">Check them out here</a>)</li>
-          <li>Going for runs</li>
-          <li>Reading (Currently reading: Memoirs of a Geisha by Arthur Golden and Clean Code by Robert Martin)</li>
-          <li>Playing video games with friends</li>
-        </ul>
+    //     <ul id="hobbies">
+    //       <h3>In my free time I enjoy:</h3>
+    //       <li>Learning Korean</li>
+    //       <li>Writing Stories (<a href="https://sarahgjacobs.github.io/sarahjacobswrites/#">Check them out here</a>)</li>
+    //       <li>Going for runs</li>
+    //       <li>Reading (Currently reading: Memoirs of a Geisha by Arthur Golden and Clean Code by Robert Martin)</li>
+    //       <li>Playing video games with friends</li>
+    //     </ul>
         
-      </section>
+    //   </section>
 
-    </section>
-  );
+    // </section>
+  <div></div>)
 }
 
 export default About;
